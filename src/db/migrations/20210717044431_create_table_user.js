517b1d3df9c338a9ca5fe593e7b2801d9af885e3
Matheus Brum <matheusdelimabrum@gmail.com>
2021-07-17 02:00:57 -0300

    adding schema, index and resolves

--- src/db/migrations/20210717044431_create_table_user.js
+++ src/db/migrations/20210717044431_create_table_user.js
@@ -1,5 +1,5 @@
 exports.up = function (knex) {
-    return knex.schema.createTable("users", table => {
+    return knex.schema.createTable("usuarios", table => {
         table.increments("id").primary()
         table.string("nome").notNull()
         table.string("email").notNull()
@@ -9,5 +9,5 @@ exports.up = function (knex) {
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTable("users");
+    return knex.schema.dropTable("usuarios");
 };
---
+++ src/graphql/modules/user/resolvers.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    Query: {
+        contatos: () => []
+    }
+}
