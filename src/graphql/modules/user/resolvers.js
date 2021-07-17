05657334beef647765cd711421068701002c062c
Matheus Brum <matheusdelimabrum@gmail.com>
2021-07-17 04:14:27 -0300

    deleting query

--- src/graphql/modules/user/resolvers.js
+++ src/graphql/modules/user/resolvers.js
@@ -2,9 +2,6 @@ const db = require("../../../db");
 
 module.exports = {
 
-    Query: {
-        usuarios: async () => await db("usuarios"),
-    },
     Mutation: {
         criarUsuario: async (_, { data }) =>
             await (await db("usuarios").insert(data).returning("*"))[0],
