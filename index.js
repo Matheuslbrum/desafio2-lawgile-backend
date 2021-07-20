423fef599cb711201fedc35ae35c42dbb790e6bf
Matheus Brum <matheusdelimabrum@gmail.com>
2021-07-20 00:16:58 -0300

    add knexfile

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const { ApolloServer } = require('apollo-server-lambda');
-const graphql = require("./src/graphql")
+const graphql = require("./src/graphql");
 
 const server = new ApolloServer({
     ...graphql,
+    /* context: (event, context, callback) => {
+
+        const user = createUser(event);
+
+        const response = {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify({
+                user: user
+            }),
+        };
+        callback(null, response);
+    }, */
 });
 
-exports.graphqlHandler = server.createHandler();
+exports.graphqlHandler = server.createHandler({
+    cors: {
+        origin: 'http://localhost:3000/',
+        credentials: true,
+    },
+});
+
+
 
