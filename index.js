0c2fa165797f679e009965147c498831e6ecd9ee
Matheus Brum <matheusdelimabrum@gmail.com>
2021-07-22 00:41:04 -0300

    last commit

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 const { ApolloServer } = require('apollo-server-lambda');
+const { response } = require('express');
 const graphql = require("./src/graphql");
+
 
 const server = new ApolloServer({
     ...graphql,
-    /* context: (event, context, callback) => {
-
-        const user = createUser(event);
-
-        const response = {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': true,
-            },
-            body: JSON.stringify({
-                user: user
-            }),
-        };
-        callback(null, response);
-    }, */
-});
-
-exports.graphqlHandler = server.createHandler({
-    cors: {
-        origin: 'http://localhost:3000/',
-        credentials: true,
+    formatError: error => {
+        console.log(error)
+        return error;
     },
+    formatResponse: response => {
+        console.log(response)
+        return response;
+    },
+    context: ({ event, context }) => ({
+        headers: event.headers,
+        functionName: context.functionName,
+        event,
+        context
+    }),
+    playground: true,
+    tracing: true
 });
 
-
-
+exports.graphqlHandler = (event, context, callback) => {
+    const handler = server.createHandler({
+        cors: {
+            origin: '*',
+            credentials: true,
+            methods: [
+                "POST",
+                "GET"
+            ],
+            allowHeaders: [
+                "Content-Type",
+                "Origin",
+                "Accept"
+            ]
+        },
+    });
+    return handler(event, context, callback);
+};
